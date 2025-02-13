ba4caa3624b3bcc385e24dae24309cdaa0ea7eeb
Annabelle Feldman <afeldman2025@kehillahstudent.org>
2025-02-13 11:13:23 -0800

    milestone 1

--- my_bot.py
+++ my_bot.py
@@ -10,7 +10,13 @@ This function will be called every time anyone says anything on a channel where 
 def should_i_respond(user_message, user_name):
   if "robot" in user_message:
     return True
-  else:
+  if "follow what I do" == user_message:
+    return True
+  if "what sport do you like to play" in user_message:
+    return True 
+  if "what is your favorite color?" in user_message:
+    return True
+  else: 
     return False
 
 """
@@ -23,5 +29,9 @@ This function will be called every time the `should_i_respond` function returns 
 * You can have the bot respond differently to different messages and users
 """
 def respond(user_message, user_name):
-  return f"""you said my name!!
-  {user_message.replace("robot", user_name)}"""
+  if "follow what I do" == user_message:
+    return "ok, tracking in progress "
+  if "what sport do you like to play" in user_message:
+    return "soccer"  
+  if "what is your favorite color?" in user_message: 
+    return "blue" 
